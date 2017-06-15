2bf9686921908f1fc0c547a14b275a8466f965bb
Manan Garg <manangarg899@gmail.com>
2017-06-15 22:54:01 +0530

    Solution Day 1 added

---
+++ Day 1/Day 1.c
@@ -0,0 +1,23 @@
+
+    // Declare second integer, double, and String variables.
+    int a;
+    double b;
+    char c[1000];
+    scanf("%d",&a);
+    scanf("%lf",&b);
+    getchar();
+    scanf("%[^\n]s",c);
+
+    printf("%d\n",a+i);
+    printf("%0.1lf\n",d+b);
+    printf("%s%s",s,c);
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+
+    // Print the sum of the double variables on a new line.
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+
