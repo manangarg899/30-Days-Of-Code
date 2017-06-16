c050252beecd56fa09d03cf619fa94d8ba2a95b4
Manan Garg <manangarg899@gmail.com>
2017-06-16 23:54:52 +0530

    Solution Day 1 added

---
+++ Day 1/Day 1.cpp
@@ -0,0 +1,22 @@
+    int a;
+    double b;
+    char t[1000];
+    cin>>a>>b;
+    cin.ignore();
+    cin.getline(t,1000);
+    cout<<a+i<<endl;
+    cout<<fixed<<setprecision(1)<<b+d<<endl;
+    cout<<s<<t;
+
+    // Declare second integer, double, and String variables.
+
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+
+    // Print the sum of both integer variables on a new line.
+
+    // Print the sum of the double variables on a new line.
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
