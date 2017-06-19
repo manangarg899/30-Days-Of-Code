1298722cd0053360057020e490ea9797c663a687
Manan Garg <manangarg899@gmail.com>
2017-06-19 21:09:39 +0530

    Added Day 3 Solution

---
+++ Day 3/Day 3.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+if(n%2==0){
+    if(n>=2 && n<=5)
+        printf("Not Weird");
+    else if(n>=6 && n<=20)
+        printf("Weird");
+    else if(n>20)
+        printf("Not Weird");
+}
+else
+    printf("Weird");
+    return 0;
+}
